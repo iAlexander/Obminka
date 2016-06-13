7d4ca6fbe3d886878ca81d6c693ff8d9738fb9ab
Alexander Iashchuk <alexander@iashchuk.com>
2016-06-13 11:59:27 +0300

    Added the Firebase analytics

--- obminka/AppDelegate.swift
+++ obminka/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
 		// Override point for customization after application launch.
+		FIRApp.configure()
 		return true
 	}
 
