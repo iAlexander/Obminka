cd74c58fa55caaa9284470ad5ce12acb2c617a64
iAlexander <alexander@iashchuk.com>
2016-07-25 23:33:36 +0300

    Removed all dependencies, updated the build version.

--- obminka/AppDelegate.swift
+++ obminka/AppDelegate.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 import CoreData
-import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,7 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
 		// Override point for customization after application launch.
-		FIRApp.configure()
 		return true
 	}
 
