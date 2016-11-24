0c44cf04b248bedc889a85271692cad7bffcd1bb
iAlexander <alexander@iashchuk.com>
2016-11-24 14:45:38 +0200

    Updated to Swift 3

--- obminka/ViewController.swift
+++ obminka/ViewController.swift
@@ -7,40 +7,12 @@
 //
 
 import UIKit
-import CloudKit
 
 class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
-
-        var artworkRecordID = CKRecordID(recordName: UIDevice.currentDevice().identifierForVendor!.UUIDString)
-        var artworkRecord = CKRecord(recordType: "Artwork", recordID: artworkRecordID)
-        artworkRecordID = CKRecordID(recordName: "Rates")
-        artworkRecord = CKRecord(recordType: "EUR", recordID: artworkRecordID)
-        artworkRecord["user"] = "Alexander"
-        artworkRecord["Title"] = "Sir"
-        var myContainer = CKContainer.defaultContainer()
-        var publicDatabase = myContainer.publicCloudDatabase
-        var privateDatabase = myContainer.privateCloudDatabase
-        
-        publicDatabase.saveRecord(artworkRecord) { (artworkRecord, error) in
-            if (error == nil) {
-                NSLog("1publicDatabase: Successfully saved record!")
-            } else {
-                NSLog("1publicDatabase: ERROR!")
-            }
-        }
-        
-        privateDatabase.saveRecord(artworkRecord) { (artworkRecord, error) in
-            if (error == nil) {
-                NSLog("2privateDatabase: Successfully saved record!")
-            } else {
-                NSLog("2privateDatabase: ERROR!")
-            }
-        }
-        
 	}
 
 	override func didReceiveMemoryWarning() {
