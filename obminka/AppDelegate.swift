bb025368336e0d8047097f32a4ec3879522ffd9e
iAlexander <alexander@iashchuk.com>
2016-11-27 14:22:23 +0200

    Updated sticker pack assets

--- obminka/AppDelegate.swift
+++ obminka/AppDelegate.swift
@@ -12,8 +12,9 @@ import CoreData
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-	var window: UIWindow?
-
+    var window: UIWindow?
+    
+    // MARK: - Application settings
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
